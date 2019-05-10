9765771ecc3bf98f87b078f54cc34a65b23cb728
kyo <r.kyo0821@icloud.com>
2019-05-10 09:31:04 +0000

    修正最後

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :edit, :show]
-  
+  before_action :correct_user, only: [:destroy, :edit, :show, :update]
+
   def index
     @tasks = Task.all
   end
